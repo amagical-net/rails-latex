ecc6984e988850e1ee8d1ea43ffea84104a796c9
jacott <geoffjacobsen@gmail.com>
2010-06-07 16:56:31 +1200

    fixed regression in lib/erb_latex.rb adding missing setup step to README.rdoc

--- lib/erb_latex.rb
+++ lib/erb_latex.rb
@@ -3,18 +3,16 @@ require 'latex_to_pdf'
 require 'action_view'
 
 module ActionView               # :nodoc: all
-  module Template
-    module Handlers
-      class ERBLatex < ERB
-        def compile(template)
-          erb = "<% __in_erb_template=true %>#{template.source}"
-          out=self.class.erb_implementation.new(erb, :trim=>(self.class.erb_trim_mode == "-")).src
-          out + ";LatexToPdf.generate_pdf(@output_buffer.to_s)"
-        end
+  module Template::Handlers
+    class ERBLatex < ERB
+      def compile(template)
+        erb = "<% __in_erb_template=true %>#{template.source}"
+        out=self.class.erb_implementation.new(erb, :trim=>(self.class.erb_trim_mode == "-")).src
+        out + ";LatexToPdf.generate_pdf(@output_buffer.to_s)"
       end
     end
-    register_template_handler :erbtex, ERBLatex
   end
+  Template.register_template_handler :erbtex, Template::Handlers::ERBLatex
 end
 
 
