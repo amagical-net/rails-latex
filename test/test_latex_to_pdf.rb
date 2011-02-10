2f304dca11612620c68cc5f2a745db9ffafb4b02
jacott <geoffjacobsen@gmail.com>
2011-02-10 14:34:35 +1300

    tidy up use of expand_path

--- test/test_latex_to_pdf.rb
+++ test/test_latex_to_pdf.rb
@@ -2,7 +2,7 @@ require 'helper'
 require 'erb_latex'
 require 'ostruct'
 
-Rails=OpenStruct.new(:root => File.dirname(TMP_DIR=File.expand_path(File.join(__FILE__,'..','tmp'))))
+Rails=OpenStruct.new(:root => TMP_DIR=File.expand_path("../tmp",__FILE__))
 
 class TestLatexToPdf < Test::Unit::TestCase
   def test_escape
@@ -15,7 +15,7 @@ class TestLatexToPdf < Test::Unit::TestCase
   def test_generate_pdf
     FileUtils.mkdir_p(TMP_DIR)
     File.open(pdf_file=File.join(TMP_DIR,'out.pdf'),'wb') do |wio|
-      wio.write(LatexToPdf.generate_pdf(IO.read(File.expand_path(File.join(__FILE__,'..','test_doc.tex')))))
+      wio.write(LatexToPdf.generate_pdf(IO.read(File.expand_path('../test_doc.tex',__FILE__))))
     end
     assert_equal "hello world\n\n1\n\n\f", `pdftotext #{pdf_file} -`
 
