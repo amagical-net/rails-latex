9aace60b0a77f6de9ef0ce4b16d64fee1d9abfdc
jacott <geoffjacobsen@gmail.com>
2012-03-21 10:03:13 +1300

    updated example to rails 3.2.2. Improved examples of forcing format and using render_to_string.

--- examples/rails-latex-demo/app/controllers/latex_example_controller.rb
+++ examples/rails-latex-demo/app/controllers/latex_example_controller.rb
@@ -3,17 +3,19 @@ class LatexExampleController < ApplicationController
   end
 
   def barcode
-    render :layout => 'barcode'
+    render :layout => 'barcode', formats: [:pdf]
   end
 
   def barcode_as_string
-    old_formats = formats
-    self.formats = ['pdf']
-    @pdf=render_to_string(action: 'barcode', layout: "barcode")
+
+    old_content_type = self.content_type
+    @pdf=render_to_string(action: 'barcode', layout: "barcode", formats: [:pdf])
+    self.content_type = old_content_type # render_to_string changes content_type
+
     File.open(file="#{Rails.root}/tmp/a.pdf",'w:binary') do |io|
       io.write(@pdf)
     end
-    self.formats = old_formats
+
     render text: "wrote #{file}"
   end
 end
